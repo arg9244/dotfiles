1a27c6da644f0a46c86d336189be46ebc79c4514
arg9244 <arg9244@gmail.com>
2025-09-12 01:43:38 +0330

    Update .config/MangoHud/MangoHud.conf Add .config/chez.txt Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,6 +1,9 @@
 # source /usr/share/cachyos-fish-config/cachyos-config.fish
 starship init fish | source
 
+# Add ~/bin to the PATH
+set -gx PATH $HOME/bin $PATH
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
