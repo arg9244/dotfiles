773a46aeef373b5c90ff4337ba392b112920db09
arg9244 <arg9244@users.noreply.github.com>
2026-07-10 02:28:13 +0330

    Update dotfiles

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,4 +1,5 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
+starship init fish | source
 
 # overwrite greeting
 # potentially disabling fastfetch
@@ -6,6 +7,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #    # smth smth
 #end
 alias purge="sudo paru -Rnscu"
+alias clean="sudo crunchycleaner -a"
 
 # chezmoi: re-add changes to already-tracked files from their original locations
 # (e.g. changes made directly to ~/.config/somefile get synced to the chezmoi source dir)
@@ -13,3 +15,4 @@ alias chezreadd="chezmoi re-add"
 
 # chezmoi: commit all changes in the source dir and push to the configured remote (GitHub)
 alias chezpush="chezmoi git -- add -A . && chezmoi git -- commit -m 'Update dotfiles' && chezmoi git -- push"
+starship init fish | source
--- install.sh
+++ install.sh
@@ -36,12 +36,13 @@ ORIGINAL_HOME="$(eval echo ~$ORIGINAL_USER)"
 
 PACMAN=(
     chromium foot ntfs-3g nautilus file-roller mpv loupe
-    git ripgrep bottom yazi python nodejs yt-dlp telegram-desktop zed
+    git ripgrep bottom yazi python nodejs yt-dlp ayugram-desktop zed
     chezmoi profile-sync-daemon power-profiles-daemon bpftune
     dmemcg-booster vkd3d lutris lxsession nicotine+
     goverlay github-cli mpv-mpris playerctl gpu-screen-recorder
     xdg-desktop-portal xdg-desktop-portal-gnome ttf-jetbrains-mono-nerd
-    gnome-keyring nm-connection-editor
+    gnome-keyring nm-connection-editor baobab amberol
+    starship ttf-nerd-fonts-symbols-common nautilus-open-any-terminal
 )
 
 AUR=(
