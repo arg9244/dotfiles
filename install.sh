5208bd5dd1b12746f0666fb1752233cf772dd6af
arg9244 <arg9244@gmail.com>
2026-08-25 10:39:08 +0330

    Update dotfiles

--- install.sh
+++ install.sh
@@ -54,14 +54,12 @@ export PATH="$ORIGINAL_HOME/.local/bin:$PATH"
 
 PACMAN=(
     niri noctalia noctalia-greeter lxsession gnome-keyring
-    xdg-desktop-portal-gnome  loupe kitty bottom chezmoi yazi
+    xdg-desktop-portal-gnome loupe kitty bottom chezmoi yazi
     icoutils lact gvfs-mtp zed paru python-mutagen nwg-look
     mpv mpv-mpris playerctl yt-dlp amberol mangohud chromium
     github-cli icoextract proton-cachyos-native winetricks
     ayugram-desktop qbittorrent aria2 blocky adw-gtk-theme
-
-    baobab file-roller gnome-disk-utility lutris uv nodejs neovim
-    rclone bleachbit
+    baobab file-roller gnome-disk-utility umu-launcher wine
 )
 
 AUR=(
